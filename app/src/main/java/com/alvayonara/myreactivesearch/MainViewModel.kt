30101cb3c45b7699e59b690a17c9ece7c52afdf5
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-09-13 13:13:42 +0700

    Create MainViewModel.kt

---
+++ app/src/main/java/com/alvayonara/myreactivesearch/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.alvayonara.myreactivesearch
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.alvayonara.myreactivesearch.network.ApiConfig
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+class MainViewModel : ViewModel(){
+
+    private val accessToken = "pk.eyJ1IjoiYWx2YXlvbmFyYSIsImEiOiJja2Ywamp3b20wa2M5MnpxZDA2cGYybzliIn0.cW4t0o0vub1wYGEpOikcZA"
+    val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    val searchResult = queryChannel.asFlow()
+        .debounce(300)
+        .distinctUntilChanged()
+        .filter {
+            it.trim().isNotEmpty()
+        }
+        .mapLatest {
+            ApiConfig.provideApiService().getCountry(it, accessToken).features
+        }
+        .asLiveData()
+}
